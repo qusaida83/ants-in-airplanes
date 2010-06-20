69f0f3a2ea2c6ef1d49e442bb716636153c5d8d8
fotanus <fotanus@ff125c2e-8409-c1bf-5e64-922e317dda43>
2010-06-20 00:39:07 +0000

    Some modifications on generate_solution let's throw this stuff on garbage and start over.

--- trunk/ants_in_planes.c
+++ trunk/ants_in_planes.c
@@ -24,20 +24,23 @@ void initialize_ant(int ant_n){
 
 }
 
+void ant_talks(int i){
+	int j;
+	printf("Ant %d reports:\n",i);
+	printf("Ant%d: My solution was %d!\n",i,ants[i].solution);
+	printf("Ant%d: I did the following path:\n",i);
+	for(j=0; j<planes_n; j++)
+		printf("\tPlane %d\n",ants[i].planes_path[j]);
+	printf("Ant %d salutes and was dispensed\n\n");
+}
 
 
 generate_solutions(){
 	puts("====NEW ROUND====");
 	int i,j,k,l;
-	int planes_visited = 0;
-	int current_plane = starting_plane;
 
-	int best_early_time = airplanes[current_plane].target_lt-1;
-	int best_late_time = airplanes[current_plane].target_lt+1;
-	int target_time = airplanes[current_plane].target_lt;
-	int occuped_late_time = 0;
-	int occuped_early_time = 0;
-	int invalid_solution = 0;
+	int occuped_late_time = 0; //this two variables must always be set before used. They are
+	int occuped_early_time = 0;//just alias to help readability. 
 
 	//be ready for the magic.
 	//
@@ -46,37 +49,50 @@ generate_solutions(){
 	//
 	//you were warned.
 	for(i=0; i<ants_n; i++){ //for each ant
+
+		//setup environment for next ant
 		initialize_ant(i);
+		unsigned short int planes_visited = 0;
+		unsigned char invalid_solution = 0;
+		unsigned short int current_plane = starting_plane;
+		unsigned int best_early_time = airplanes[current_plane].target_lt-1;
+		unsigned int best_late_time = airplanes[current_plane].target_lt+1;
+		unsigned int target_time = airplanes[current_plane].target_lt;
+
 		while(planes_visited < planes_n){ //for each plane
-			ants[i].planes_path[planes_visited] = current_plane;
+
+			ants[i].planes_path[planes_visited] = current_plane; //keeps the trace of visited planes.
+
 			for(k=0; k<planes_n; k++){ // for each other plane ...
 				if( ants[i].planes_lt[k] > 0){ // ... that was already visited
 
-					//some alias to help 
+					//some alias to help
+					//delimites the range which the plane can't land.
 					occuped_early_time = ants[i].planes_lt[k] - separation_time[current_plane][k];
 					occuped_late_time = ants[i].planes_lt[k] + separation_time[current_plane][k];
 
-
-					//case 1: the plane analized landed after the current target landing time
+					//case 1: the plane analized landed before the current target landing time
 					if( airplanes[current_plane].target_lt > ants[i].planes_lt[k]){
-						best_early_time = occuped_late_time;
 						if( best_early_time != -1){
+							if(occuped_late_time > best_early_time)
+								best_early_time = occuped_late_time;
 
-							if(occuped_early_time > best_early_time)
-								best_early_time = occuped_early_time;
-
-							if(best_early_time > airplanes[current_plane].target_lt){
+							//this checks for overflow; i.e. when the analized plane landing time
+							//get to or trespass the current plane target landing time.
+							if(best_early_time == airplanes[current_plane].target_lt)
+								best_early_time = -1;
+							else if(best_early_time > airplanes[current_plane].target_lt){
 								target_time = -1;
 								best_early_time = -1;
 							}
 						}
 						if(best_early_time == -1){
-							if( best_early_time > best_late_time)
-								best_late_time = best_early_time;
+							if( occuped_late_time > best_late_time)
+								best_late_time = occuped_late_time;
 						}
 					}
 
-					//case 2: the plane analized landed before the current target landing time
+					//case 2: the plane analized landed after the current target landing time
 					else if( airplanes[current_plane].target_lt < ants[i].planes_lt[k]){
 						if( best_late_time != -1){
 
@@ -93,6 +109,7 @@ generate_solutions(){
 								best_early_time = best_late_time;
 						}
 					}
+
 					//case 3: the plane analized landed EXACTLY on the current target landing time
 					else{
 						if(best_early_time != -1)
@@ -101,12 +118,13 @@ generate_solutions(){
 							best_late_time = occuped_late_time;
 						target_time = -1;
 					}
-				}
+				} // end if was visited
+			} // end to analize the conflict time with this plane.
 
+			//at this point, for the current_plane, all other planes that
+			//had the landing time already decided were checked.
 
-			}
 			//decides what is the best solution for this plane
-			//
 			if(target_time != -1) //can use target time
 				ants[i].planes_lt[current_plane] = target_time;
 			else if(best_early_time != -1)
@@ -121,12 +139,12 @@ generate_solutions(){
 			else if(best_late_time != -1) //can only pick late
 				ants[i].planes_lt[current_plane] = best_late_time;
 			else{ //solution was invalid
-				puts("Nooo! Invalid solution!!");
+				printf("Ant%d: Nooo! Invalid solution! I want to be dispensed!\n",i);
 				invalid_solution = 1;
 				break;
 			}
 
-			printf("Ant%d: For the plane %d I've choosed the time %d.\n",i,current_plane, ants[i].planes_lt[current_plane]);
+			//printf("Ant%d: For the plane %d I've choosed the time %d.\n",i,current_plane, ants[i].planes_lt[current_plane]);
 			++planes_visited;
 
 			int next_plane;
@@ -143,30 +161,20 @@ generate_solutions(){
 
 		}
 
+		//if the solutions is valid, calculate stuff
 		if(invalid_solution == 0){
-			//calculate the solution
 			for(j=0; j<planes_n; j++){
 				if(ants[i].planes_lt[j] < airplanes[j].target_lt)
 					ants[i].solution += (airplanes[j].target_lt - ants[i].planes_lt[j]) * airplanes[j].cost_before;
 				else if(ants[i].planes_lt[j] > airplanes[j].target_lt)
 					ants[i].solution += (ants[i].planes_lt[j] - airplanes[j].target_lt ) * airplanes[j].cost_after;
+				//else is the target landing time and don't chaes the solution
 			}
-			printf("Ant%d: My solution was %d!\n",i,ants[i].solution);
-			printf("Ant%d: I did this path:\n",i);
-			for(j=0; j<planes_n; j++)
-				printf("\tPlane %d\n",ants[i].planes_path[j]);
+			ant_talks(i);
 		}
+		else
+			--i; //same ant try again
 
-		if(invalid_solution == 1)
-			i--; //same ant try again
-
-		//setup environment for next ant
-		planes_visited = 0;
-		invalid_solution = 0;
-		current_plane = starting_plane;
-		best_early_time = airplanes[current_plane].target_lt-1;
-		best_late_time = airplanes[current_plane].target_lt+1;
-		target_time = airplanes[current_plane].target_lt;
 	}
 
 }
@@ -218,7 +226,7 @@ int not_end(){
 
 
 int main(int argc, const char *argv[]){
-	parser("instance/airland9.txt");
+	parser("instance/airland1.txt");
 	//print_extracted_data();
 
 	setup_parameters();
